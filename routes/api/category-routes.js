a6a120c3fd59776892ed572cc00da19f95c570e0
Vivid Quill <sjdoublec@gmail.com>
2023-09-14 11:14:41 -0500

    category-routes.js and tag-routes.js seem good to go. Need to finish product-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,144 +1,83 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-
 // Routes at the `/api/categories` endpoint:
 
 // GET all categories:
 router.get('/', async (req, res) => {
-  const categoryData = await Category.findAll(
-    {
-      include: {
-        model: Product,
-        attributes: ['product_name']
-      }
-    }
-  );
-  return res.json(categoryData);
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-// GET one category by its 'id' value:
+// GET a category by its 'id' value:
 router.get('/:id', async (req, res) => {
-  const categoryData = await Category.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: {
-      model: Product,
-      attributes: ['product_name']
-    }
-  });
-  return res.json(categoryData);
+  try {
+    const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{ model: Product }]
+    });
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // CREATE a new category:
 router.post('/', async (req, res) => {
-  const categoryData = await Category.create(req.body);
-  return res.json(categoryData);
+  try {
+    const categoryData = await Category.create(req.body);
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // UPDATE a category by its 'id' value:
 router.put('/:id', async (req, res) => {
-  const categoryData = await Category.update(
-    {
-      id: req.body.id,
-      category_name: req.body.category_name,
-    },
-    {
-      where: {
-        id: req.params.id,
+  try {
+    const categoryData = await Category.update(
+      {
+        id: req.body.id,
+        category_name: req.body.category_name
       },
-    }
-  );
-  return res.json(categoryData);
+      {
+        where: {
+          id: req.params.id
+        },
+      }
+    );
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // DELETE a category by its 'id' value:
 router.delete('/:id', async (req, res) => {
-  const categoryData = await Category.destroy(
-    {
-      where: {
-        id: req.params.id,
-      },
-    });
-  return res.json(categoryData);
+  try {
+    const categoryData = await Category.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
-
-
-// // GET all categories using '.then()' syntax:
-// router.get('/', (req, res) => {
-//   Category.findAll().then((categoryData) => {
-//     res.json(categoryData);
-//   });
-//   // Include its associated Products (what does this mean?):
-// });
-
-
-// // GET category by its 'id' value using '.then()' syntax:
-// router.get('/:id', (req, res) => {
-//   Category.findByPk(req.params.id).then((categoryData) => {
-//     res.json(categoryData);
-//   });
-//   // Include its associated Products (what does this mean?):
-// });
-
-// // Another type of GET request. This one still uses '.then()' syntax:
-// // How is it different from the GET request directly above it?
-// router.get('/:id', (req, res) => {
-//   Category.findOne(
-//     {
-//       where: {
-//         id: req.params.id
-//       },
-//     }
-//   ).then((categoryData) => {
-//     res.json(categoryData);
-//   });
-// });
-
-
-// // CREATE a new category using '.then()' syntax:
-// router.post('/', (req, res) => {
-//   Category.create(req.body)
-//     .then((newCategory) => {
-//       res.json(newCategory);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
-
-
-// // UPDATE a category by its `id` value using '.then()' syntax:
-// router.put('/:id', (req, res) => {
-//   Category.update(
-//     {
-//       id: req.body.id,
-//       category_name: req.body.category_name,
-//     },
-//     {
-//       where: {
-//         id: req.params.id,
-//       },
-//     }
-//   )
-//   .then((updatedCategory) => {
-//     res.json(updatedCategory);
-//   })
-//   .catch((err) => res.json(err));
-// });
-
-
-// // Delete a category by its `id` value using '.then()' syntax:
-// router.delete('/:id', (req, res) => {
-//   Category.destroy({
-//     where: {
-//       id: req.params.id,
-//     }
-//   })
-//     .then((deletedCategory) => {
-//       res.json(deletedCategory);
-//     })
-//     .catch((err) => res.json(err));
-// });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,130 +1,84 @@
 const router = require('express').Router();
+// Not sure why 'ProductTag' model was included in this file:
 const { Tag, Product, ProductTag } = require('../../models');
-
 // Routes at the `/api/tags` endpoint:
 
 // GET all tags:
 router.get('/', async (req, res) => {
-  const tagData = await Tag.findAll();
-  return res.json(tagData);
-  // Include its associated Product data (what does this mean?):
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.json(tagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // GET a tag by its 'id':
 router.get('/:id', async (req, res) => {
-  const tagData = await Tag.findByPk(req.params.id);
-  return res.json(tagData);
-  // Include its associated Product data (what does this mean?):
+  try {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{ model: Product }]
+    });
+    res.json(tagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // CREATE a new tag:
 router.post('/', async (req, res) => {
-  const tagData = await Tag.create(req.body);
-  return res.json(tagData);
+  try {
+    const tagData = await Tag.create(req.body);
+    res.json(tagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-// UPDATE a tag's name by its `id` value:
+// UPDATE a tag name by its `id` value:
 router.put('/:id', async (req, res) => {
-  const tagData = await Tag.update(
-    {
-      id: req.body.id,
-      tag_name: req.body.tag_name,
-    },
-    {
-      where: {
-        id: req.params.id,
+  try {
+    const tagData = await Tag.update(
+      {
+        id: req.body.id,
+        tag_name: req.body.tag_name
       },
-    }
-  );
-  return res.json(tagData);
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(tagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // DELETE a tag by its `id` value:
 router.delete('/:id', async (req, res) => {
-  const tagData = await Tag.destroy(
-    {
-      where: {
-        id: req.params.id,
-      },
-    });
-  return res.json(tagData);
+  try {
+    const tagData = await Tag.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(tagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
-
-// // GET all tags using '.then()' syntax:
-// router.get('/', (req, res) => {
-//   Tag.findAll().then((tagData) => {
-//     res.json(tagData);
-//   });
-//   // Include its associated Product data (what does this mean?):
-// });
-
-
-// // GET a tag by its 'id' using '.then()' syntax:
-// router.get('/:id', (req, res) => {
-//   Tag.findByPk(req.params.id).then((tagData) => {
-//     res.json(tagData);
-//   })
-//   // Include its associated Product data (what does this mean?):
-// });
-
-// // Will this GET request work better for one particular category 'id'?
-// // How is it different from the GET request directly above it?
-// router.get('/:id', (req, res) => {
-//   Tag.findOne(
-//     {
-//       where: {
-//         id: req.params.id
-//       },
-//     }
-//   ).then((tagData) => {
-//     res.json(tagData);
-//   });
-// });
-
-
-// // CREATE a new tag using '.then()' syntax:
-// router.post('/', (req, res) => {
-//   Tag.create(req.body)
-//     .then((newTag) => {
-//       res.json(newTag);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
-
-
-// // UPDATE a tag's name by its `id` value using '.then()' syntax:
-// router.put('/:id', (req, res) => {
-//   Tag.update(
-//     {
-//       id: req.body.id,
-//       tag_name: req.body.tag_name,
-//     },
-//     {
-//       where: {
-//         id: req.params.id,
-//       },
-//     }
-//   )
-//   .then((updatedTag) => {
-//     res.json(updatedTag);
-//   })
-//   .catch((err) => res.json(err)) ;
-// });
-
-
-// // DELETE a tag by its `id` value using '.then()' syntax:
-// router.delete('/:id', (req, res) => {
-//   Tag.destroy({
-//     where: {
-//       id: req.params.id,
-//     }
-//   })
-//     .then((deletedTag) => {
-//       res.json(deletedTag);
-//     })
-//     .catch((err) => res.json(err));
-// });
